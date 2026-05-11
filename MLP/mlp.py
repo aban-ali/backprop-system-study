78c40d8c12fb0e3aee853975edb386efaa81584f
aban-ali <22f1001086@ds.study.iitm.ac.in>
2026-05-11 18:56:23 +0000

    Started woking with simple MLP version

---
+++ MLP/mlp.py
@@ -0,0 +1,14 @@
+""" This file contains the pytorch implementation
+of simple multi layer perceptron architecture as follows - 
+Inputs->Linear->ReLU->Linear->ReLU->Linear->Softmax """
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+class MLP(nn.module):
+    def __init__(self):
+        pass
+    
+    def forward(self):
+        pass
