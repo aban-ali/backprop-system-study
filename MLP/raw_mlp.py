02dd7963926c9f0fb9773f253f043f92b63f5bdc
aban-ali <22f1001086@ds.study.iitm.ac.in>
2026-05-18 11:05:18 +0000

    Backprop and model validation code left

--- MLP/raw_mlp.py
+++ MLP/raw_mlp.py
@@ -19,16 +19,14 @@ class CrossEntropyCost():
 class HelperFunc():
 
     @staticmethod
-    def sigmoid(z):
-        """Sigmoid Value."""
-        return 1.0 / ( 1.0 + np.exp(-z) )
+    def relu(z):
+        """ReLU Value."""
+        return np.maximum(z, 0)
     
     @staticmethod
-    def sigmoid_prime():
-        """Derivative of sigmoid."""
-        pass
-
-
+    def relu_prime(z):
+        """Derivative of ReLU."""
+        return (z > 0).astype(float)
 
 
 class NeuralNet():
@@ -50,7 +48,7 @@ class NeuralNet():
         
         """
         self.biases = [ np.random.randn(size, 1) for size in self.sizes[1:] ]
-        self.weights = [ np.random.randn(x, y) 
+        self.weights = [ np.random.randn(y, x) 
                                 for x, y in zip(self.sizes[:-1], self.sizes[1:]) ]
         
     def feedforward(self, x):
@@ -58,10 +56,81 @@ class NeuralNet():
         from Neural Network after a forward pass.
         
         """
-        for w,b in zip(self.weights, self.biases):
-            x = HelperFunc.sigmoid( np.dot(w, x) + b )
+        for w,b in zip(self.weights[:-1], self.biases[:-1]):
+            x = HelperFunc.relu( np.dot(w, x) + b )
+        x = np.dot(self.weights[-1], x) + self.biases[-1]
         return x
 
-    def SGD(self):
+    def SGD(self, data, epochs=3, batch_size=20, lr=0.005):
         """"""
+        for epoch in range(epochs):
+            print(f"Epoch {epoch} started....")
+            images, labels = data
+            size = len(images)
+            mini_batches = []
+
+            for i in range(0, size, batch_size):
+                mini_batches.append((images[i:i+batch_size], labels[i:i+batch_size]))
+
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, lr)
+
+    def update_mini_batch(self, mini_batch, lr=0.005):
+        images, labels = mini_batch
+        grad_w = [np.zeros(w.shape) for w in self.weights]
+        grad_b = [np.zeros(b.shape) for b in self.biases]
+        for x, y in zip(images, labels):
+            delta_w, delta_b = self.backprop(x, y)
+            grad_w += delta_w
+            grad_b += delta_b
+        self.weights -= ( lr / len(mini_batch) ) * grad_w
+        self.biases -= ( lr / len(mini_batch) ) * grad_b
         
+    def backprop(self, x, y):
+        delta_w = [ np.zeros(w.shape) for w in self.weights ]
+        delta_b = [ np.zeros(b.shape) for b in self.biases ]
+
+
+
+    def validate_model(self):
+        pass
+
+
+
+
+
+
+def load_data(train=True):
+    """Extract the """
+    filename = "train-images-idx3-ubyte" if train else "t10k-images-idx3-ubyte"
+    label_file = "train-labels-idx1-ubyte" if train else "t10k-labels-idx1-ubyte"
+    n = int(6e4) if train else int(1e4)
+    images = []
+    labels = []
+
+    f = open(f"./dataset/MNIST/raw/{filename}", "rb")
+    l = open(f"./dataset/MNIST/raw/{label_file}", "rb")
+
+    f.read(16)
+    l.read(8)
+
+    for _ in range(n):
+        labels.append(ord(l.read(1)))
+        image = []
+        for __ in range(28*28):
+            image.append(ord(f.read(1)))
+        images.append(image)
+
+    return (np.array(images), np.array(labels))
+
+
+
+def main():
+    nn_size = [784, 256, 128, 10]
+    net = NeuralNet(sizes=nn_size)
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
